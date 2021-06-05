f2e36fe15d2237ea0574d88b3525f5baa16ce1af
Janani <41537681+Janani0608@users.noreply.github.com>
2021-06-05 18:59:03 +0200

    Add files via upload

---
+++ Statistical NLP/OOV/plots.py
@@ -0,0 +1,39 @@
+from typing import Dict, List
+
+def plot_pp(pps: Dict):
+    """ Plots perplexity vs n for all languages in the corpus
+
+    :param pps: dictionary with langs as keys and lists of perplexity scores as values
+    """
+    c = {'en': 'blue', 'fi': 'green', 'ru':'red', 'ta': 'yellow'}
+    x = list(range(1,len(pps)+1))
+    for lang in pps.keys():
+      #Perplexity of train data
+      y = pps[lang][0]
+      plt.xlabel("n values of n-grams")
+      plt.ylabel("Perplexity for train data")
+      plt.plot(x,y, color = c[lang], legend = lang)
+      #Perplexity of test data
+      y = pps[lang][1]
+      plt.xlabel("n values of n-grams")
+      plt.ylabel("Perplexity for test data")
+      plt.plot(x,y, color = c[lang], legend = lang)
+    plt.legend()
+
+
+def plot_pp_vs_alpha(pps: List[float], alphas: List[float]):
+    """ Plots n-gram perplexity vs alpha
+    :param pps: list of perplexity scores
+    :param alphas: list of alphas
+    """
+    #Perplexity of train data for the given alpha range
+    plt.plot(alphas, pps[0])
+    plt.xlabel("Alpha values")
+    plt.ylabel("Train perplexities")
+    plt.show()
+    #Perplexity of test data for the given alpha range
+    plt.plot(alphas, pps[1])
+    plt.xlabel("Alpha values")
+    plt.ylabel("Test perplexities")
+    plt.show()
+
